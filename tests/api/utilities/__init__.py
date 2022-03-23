998559b9367652aa46efc7f08d09796ae954bed0
ks6088ts <ks6088ts@gmail.com>
2022-03-23 15:35:35 +0900

    remove tests directory

--- tests/api/utilities/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import get_api_config_on_production, get_api_config_on_sandbox  # noqa
-from .types import Coverage  # noqa
--- tests/api/utilities/types.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class Coverage(Enum):
-    JAPAN = 0
-    GLOBAL = 1
